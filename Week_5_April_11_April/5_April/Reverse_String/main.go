2694b66cf329afbb006518ceb492333b6d433b48
Rishav-R03 <rishav042023@gmail.com>
2026-04-05 16:55:49 +0530

    reverse a string

--- Week_5_April_11_April/5_April/Reverse_String/main.go
+++ Week_5_April_11_April/5_April/Reverse_String/main.go
@@ -1,1 +1,14 @@
 package main
+
+func reverseString(s []byte) {
+	for i, j := 0, len(s)-1; i < j; i++ {
+		s[i], s[j] = s[j], s[i]
+		j--
+	}
+}
+
+func main() {
+	s := []byte("hello")
+	reverseString(s)
+	println(string(s))
+}
