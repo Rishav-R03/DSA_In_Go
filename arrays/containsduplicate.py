69750703c99664964e36d1e7afa9119e53c34be9
Rishav-R03 <rishav042023@gmail.com>
2026-09-04 23:47:28 +0530

    two sum

--- arrays/containsduplicate.py
+++ arrays/containsduplicate.py
@@ -14,6 +14,15 @@ class Solution:
                 return True 
         return False
 
+    def two_sum(self,arr:list[int],target:int)->list[int]:
+        map1 = {}
+        for k,v in enumerate(arr):
+            diff = target-v 
+            if diff in map1:
+                return [map1[diff],k]
+            map1[diff] = k 
+        return []
+
 
 if __name__ == "__main__":
     arr = [1,3,1]
@@ -21,3 +30,6 @@ if __name__ == "__main__":
         print("contains duplicate")
     else:
         print("no it doesnot")
+
+    indices = Solution(arr).two_sum(arr,2)
+    print(indices)
